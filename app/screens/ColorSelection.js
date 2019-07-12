20e026457a174c62e56cc7efafbfb9224eb05697
Huberrrr <hubermjonathan@gmail.com>
2019-07-12 07:24:35 -0700

    rearranged colors on selection screen

--- app/screens/ColorSelection.js
+++ app/screens/ColorSelection.js
@@ -2,7 +2,8 @@ import React from 'react';
 import {
     StyleSheet,
     View,
-    Image
+    Image,
+    Dimensions
 } from 'react-native';
 import TextMono from '../components/TextMono';
 import { TouchableOpacity } from 'react-native-gesture-handler';
@@ -22,60 +23,68 @@ export default class ColorSelectionScreen extends React.Component {
         return (
             <View style={styles.container}>
                 <TextMono style={styles.infoText}>Choose your color</TextMono>
-                <TouchableOpacity
-                    onPress={this._navigateToCapture.bind(this, 'red')}
-                    activeOpacity={0.8}
-                >
-                    <Image
-                        style={styles.red}
-                        source={require('../../assets/red.png')}
-                    />
-                </TouchableOpacity>
-                <TouchableOpacity
-                    onPress={this._navigateToCapture.bind(this, 'orange')}
-                    activeOpacity={0.8}
-                >
-                    <Image
-                        style={styles.orange}
-                        source={require('../../assets/orange.png')}
-                    />
-                </TouchableOpacity>
-                <TouchableOpacity
-                    onPress={this._navigateToCapture.bind(this, 'green')}
-                    activeOpacity={0.8}
-                >
-                    <Image
-                        style={styles.green}
-                        source={require('../../assets/green.png')}
-                    />
-                </TouchableOpacity>
-                <TouchableOpacity
-                    onPress={this._navigateToCapture.bind(this, 'blue')}
-                    activeOpacity={0.8}
-                >
-                    <Image
-                        style={styles.blue}
-                        source={require('../../assets/blue.png')}
-                    />
-                </TouchableOpacity>
-                <TouchableOpacity
-                    onPress={this._navigateToCapture.bind(this, 'brown')}
-                    activeOpacity={0.8}
-                >
-                    <Image
-                        style={styles.brown}
-                        source={require('../../assets/brown.png')}
-                    />
-                </TouchableOpacity>
-                <TouchableOpacity
-                    onPress={this._navigateToCapture.bind(this, 'white')}
-                    activeOpacity={0.8}
-                >
-                    <Image
-                        style={styles.white}
-                        source={require('../../assets/white.png')}
-                    />
-                </TouchableOpacity>
+                <View style={styles.row}>
+                    <TouchableOpacity
+                        onPress={this._navigateToCapture.bind(this, 'red')}
+                        activeOpacity={0.8}
+                    >
+                        <Image
+                            style={styles.red}
+                            source={require('../../assets/red.png')}
+                        />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={this._navigateToCapture.bind(this, 'orange')}
+                        activeOpacity={0.8}
+                    >
+                        <Image
+                            style={styles.orange}
+                            source={require('../../assets/orange.png')}
+                        />
+                    </TouchableOpacity>
+                </View>
+
+                <View style={styles.row}>
+                    <TouchableOpacity
+                        onPress={this._navigateToCapture.bind(this, 'green')}
+                        activeOpacity={0.8}
+                    >
+                        <Image
+                            style={styles.green}
+                            source={require('../../assets/green.png')}
+                        />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={this._navigateToCapture.bind(this, 'blue')}
+                        activeOpacity={0.8}
+                    >
+                        <Image
+                            style={styles.blue}
+                            source={require('../../assets/blue.png')}
+                        />
+                    </TouchableOpacity>
+                </View>
+
+                <View style={styles.row}>
+                    <TouchableOpacity
+                        onPress={this._navigateToCapture.bind(this, 'brown')}
+                        activeOpacity={0.8}
+                    >
+                        <Image
+                            style={styles.brown}
+                            source={require('../../assets/brown.png')}
+                        />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={this._navigateToCapture.bind(this, 'white')}
+                        activeOpacity={0.8}
+                    >
+                        <Image
+                            style={styles.white}
+                            source={require('../../assets/white.png')}
+                        />
+                    </TouchableOpacity>
+                </View>
             </View>
         );
     }
@@ -85,7 +94,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'center',
+        justifyContent: 'space-evenly',
         backgroundColor: '#000000'
     },
     infoText: {
@@ -93,6 +102,12 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         textAlign: 'center',
         color: '#FFCB05'
+    },
+    row: {
+        flexDirection: 'row',
+        width: Dimensions.get('window').width,
+        height: 84 * 1.3,
+        justifyContent: 'space-evenly'
     },
     red: {
         width: 97 * 1.3,
