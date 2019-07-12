3e11c20c8cb38c08811dbf0e399c7eb15a4e18df
Shiv Paul <shivapaul98@ibm.com>
2019-07-12 05:37:44 -0700

    swiping viewpagers

--- app/screens/Info.js
+++ app/screens/Info.js
@@ -5,6 +5,7 @@ import {
 	View
 } from 'react-native';
 import TextMono from '../components/TextMono';
+import Swiper from 'react-native-swiper'
 
 export default class InfoScreen extends React.Component {
 	constructor(props) {
@@ -13,9 +14,17 @@ export default class InfoScreen extends React.Component {
 
 	render() {
 		return (
-			<View>
-				<TextMono>fwfwfwef</TextMono>
-			</View>
+			<Swiper loop={false} style={styles.mainBG} showsButtons={true}>
+				<View style={styles.mainBG}>
+					<TextMono style={styles.infoText}>Page 1</TextMono>
+				</View>
+				<View style={styles.mainBG}>
+					<TextMono style={styles.infoText}>Page 2</TextMono>
+				</View>
+				<View style={styles.mainBG}>
+					<TextMono style={styles.infoText}>Page 3</TextMono>
+				</View>
+			</Swiper>
 		);
 	}
 }
@@ -25,6 +34,13 @@ const styles = StyleSheet.create({
 		flex: 1,
 		alignItems: 'center',
 		justifyContent: 'space-around',
+	},
+	mainBG: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: '#000000'
+
 	},
 	button: {
 		backgroundColor: '#FFCB05',
@@ -36,5 +52,11 @@ const styles = StyleSheet.create({
 	buttonText: {
 		fontSize: 24,
 		fontWeight: 'bold'
-	}
+	},
+	infoText: {
+		fontSize: 22,
+		fontWeight: 'bold',
+		textAlign: 'center',
+		color: '#FFCB05'
+	},
 });
