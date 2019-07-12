ca58a21d37d9daebf83a3472aa84796d33dadb40
Huberrrr <hubermjonathan@gmail.com>
2019-07-11 23:29:37 -0700

    updated messages and interval for random messages

--- app/screens/Loading.js
+++ app/screens/Loading.js
@@ -6,47 +6,49 @@ import {
 } from 'react-native';
 import TextMono from '../components/TextMono';
 
+const MESSAGES = [
+    'Obtaining the grain...',
+    'Buying Costco pizzas...',
+    'Calling Chipotle in advance...',
+    'Identifying hotdog...',
+    'Identifying not hotdog...',
+    'Not being paid overtime...',
+    'Calling the [I]ntelligent [B]oardgame [M]asters...',
+    'Walking the longest road...',
+    'Amassing the largest army...',
+    'Building roads...',
+    'Building more pylons...',
+    'Trading for brick...',
+    'Rolling a 7...',
+    'Hardcoding your result...',
+    'Training the ML model...',
+    'Building a settlement...',
+    'Rolling the dice...',
+    'Hanging with sloths...',
+    'Stealing snacks from H105...',
+    'Getting free food...',
+    'Sitting on hold for a flight agent...',
+    'Stealing Emma from the others...',
+    'Drooling on the pillow...',
+    'Forgetting to submit etotals...',
+    'Draining your phones battery...',
+    'Explaining Catan to Caly...',
+    'Driving to LA...',
+    'Dying hair blue...'
+];
+
 export default class LoadingScreen extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            messages: [
-                'Obtaining the grain...',
-                'Buying Costco pizzas...',
-                'Calling Chipotle in advance...',
-                'Identifying hotdog...',
-                'Identifying not hotdog...',
-                'Not being paid overtime...',
-                'Calling the [I]ntelligent [B]oardgame [M]asters...',
-                'Walking the longest road...',
-                'Amassing the largest army...',
-                'Building roads...',
-                'Building more pylons...',
-                'Trading for brick...',
-                'Rolling a 7...',
-                'Hardcoding your result...',
-                'Training the ML model...',
-                'Building a settlement...',
-                'Rolling the dice...',
-                'Hanging with sloths...',
-                'Stealing snacks from H105...',
-                'Getting free food...',
-                'Sitting on hold for a flight agent...',
-                'Stealing Emma from the others...',
-                'Drooling on the pillow...',
-                'Forgetting to submit etotals...',
-                'Draining your phones battery...',
-                'Explaining Catan to Caly...',
-                'Driving to LA...',
-                'Dying hair blue...'
-            ],
-            currentMessage: ''
+            messages: MESSAGES,
+            currentMessage: MESSAGES[Math.floor(Math.random() * MESSAGES.length)]
         }
     }
 
     componentDidMount() {
-        let interval = setInterval(this._getRandomLoadingMessage.bind(this), 2000);
+        this.interval = setInterval(this._getRandomLoadingMessage.bind(this), 2000);
         this._test();
     }
 
@@ -68,7 +70,14 @@ export default class LoadingScreen extends React.Component {
         //     .then((json) => {
         //         console.log(JSON.stringify(json));
         //     });
-        console.log('here');
+
+        setTimeout(() => {
+            clearInterval(this.interval);
+            this.props.navigation.navigate('Results', {
+                color: this.props.navigation.getParam('color'),
+                photo: this.props.navigation.getParam('photo')
+            });
+        }, 6000);
     }
 
     render() {
